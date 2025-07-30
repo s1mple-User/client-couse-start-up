a68c6f66052dfcf055ab30d18d1818f30ffbc6fc
s1mple-User <u3905173@gmail.com>
2025-07-30 16:15:34 -0400

    add all project files 6

--- src/pages/shop/cart.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next';
-import { withLayout } from 'src/layouts/layout';
-import { CartPageComponent } from 'src/page-component';
-
-const CartPage: NextPage = () => {
-	return <CartPageComponent />;
-};
-
-export default withLayout(CartPage);
