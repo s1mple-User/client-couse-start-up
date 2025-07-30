2893322c5d290b7dc5f99432288ccdbb65e7d9b7
s1mple-User <u3905173@gmail.com>
2025-07-30 15:31:32 -0400

    add all project files 2

--- next.config.js
+++ next.config.js
@@ -6,9 +6,15 @@ const nextConfig = {
     domains: [
       'media.graphassets.com',
       'localhost',
-      'server-course-production-bfe4.up.railway.app', 
+      'server-course-production-bfe4.up.railway.app',
     ],
     dangerouslyAllowSVG: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true, 
   },
 };
 
